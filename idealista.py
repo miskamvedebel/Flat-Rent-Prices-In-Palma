9384bdb0b4725cf274e0696d5f61903eb128742f
Maksim Lebedev <miskamvedebel@users.noreply.github.com>
2018-01-09 07:36:31 +0100

    0.1 version

--- idealista.py
+++ idealista.py
@@ -2,66 +2,53 @@ import requests
 from bs4 import BeautifulSoup
 import numpy
 from lxml import html
-import pprint
 import pandas as pd
 import urllib
 import time
 import datetime
 import re
-import copy
 
-to_rent_idealista = "alquiler-viviendas"
-website = "https://www.idealista.com/en/"+to_rent_idealista+"/palma-de-mallorca-balears-illes/"
-idealista_page = requests.get(website)
-tree = html.fromstring(idealista_page.content)
-items = tree.xpath('//a[@class="item-link "]/text()')
-prices = tree.xpath('//span[@class="item-price"]/text()')
-#print(list(zip(items, prices)))
-
-pages = tree.xpath('//a[@class=""]/text()')
-last_page_num = pages[-1]
-link_mask = "pagina-"+last_page_num+".htm"
-last_page_link = website + link_mask
-
+def get_tree(website):
+    tree = html.fromstring(requests.get(website).content)
+    return tree
+def get_tag_class(tree, tag, class_name):
+    string = "//{}[@class={}]/text()".format(tag, class_name)
+    res = tree.xpath(string)
+    return res
+def link_builder(rent_or_buy):
+    if rent_or_buy == 'rent':
+        website = "https://www.idealista.com/en/alquiler-viviendas/palma-de-mallorca-balears-illes/"                
+    elif rent_or_buy == 'buy':
+        website = "https://www.idealista.com/en/venta-viviendas/palma-de-mallorca-balears-illes/"
+    tree = get_tree(website)
+    last_page = get_tag_class(tree, "a", '""')[-1]
+    pages = []
+    for i in range(1,int(last_page)+1):
+        pages.append("{}pagina-{}.htm".format(website, i))
+    return pages, last_page  
 #Finding all pages from the website
-all_pages = []
+all_pages, last_page = link_builder('rent')
 
-for i in range(1,int(last_page_num)+1):
-    website_mask = "https://www.idealista.com/en/alquiler-viviendas/palma-de-mallorca-balears-illes/pagina-"+str(i)+".htm"
-    all_pages.append(website_mask)
-
-#List of item - all pages
-
-all_items = list()
-all_items_flats = list()
-all_items_prices = list()
 lp = list()
 links = list()
 details = list()
-p = 0
-
+columns = ['Flat', 'Price']
+df = pd.DataFrame(data=numpy.zeros((0,len(columns))), columns=columns)
 for t in all_pages:
-
-    p=p+1 #Page counter
-    page_content = requests.get(t)
-    page_content_tree = html.fromstring(page_content.content)
-    items_1 = page_content_tree.xpath('//a[@class="item-link "]/text()')
-    prices_1 = page_content_tree.xpath('//span[@class="item-price"]/text()')
-    all_items_flats.append(items_1)
-    all_items_prices.append(prices_1)
-    all_items.append(list(zip(items_1,prices_1)))
-
-    #Getting items links and id
-    idlst = urllib.request.urlopen(t)
-    soup = BeautifulSoup(idlst)
+    df_temp = pd.DataFrame(data=numpy.zeros((0,len(columns))), columns=columns)
+    page_content_tree = get_tree(t)
+    df_temp['Flat'] = get_tag_class(page_content_tree, "a", '"item-link "')
+    df_temp['Price'] = get_tag_class(page_content_tree, "span", '"item-price"')
+    df = pd.concat([df, df_temp], ignore_index = True)
+    soup = BeautifulSoup(urllib.request.urlopen(t))
     all_items_links = soup.find_all("a", class_="item-link ")
+    all_div_tag = soup.find_all("div", class_="item-info-container")
     for l in all_items_links:
         tp = l.get("href")
         full_link_to_flat = "https://www.idealista.com"+tp
-        id_ = tp[13:21]
+        id_ = tp.split('/')[3]
         links.append(full_link_to_flat)
         lp.append(id_)
-    all_div_tag = soup.find_all("div", class_="item-info-container")
     for tag in all_div_tag:
         a_tag = tag.find("a", class_="item-link ")
         a_tag_href = a_tag.get("href")
@@ -70,24 +57,11 @@ for t in all_pages:
         for span in span_tag:
             span_txt = span.text
             details.append(span_txt)
-#Creating empty data set
-columns = ['Flat', 'Price', 'Id', 'Link']
-df = pd.DataFrame(data=numpy.zeros((0,len(columns))), columns=columns)
-
-for i in range(0,p):
-    df_i = pd.DataFrame(all_items_flats[i], columns=["Flat"])
-    df_i["Price"] = all_items_prices[i]
-    df = df.append(df_i, ignore_index=True)
 df["Id"] = lp
 df["Link"] = links
 df["Timestamp"] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
 
-tsmp = time.time()
-st = datetime.datetime.fromtimestamp(tsmp).strftime('%Y-%m-%d %H%M%S')
-filenamelocation = "C:/Users/maksim.lebedev/Desktop/idealista/idealista-rent-"+st+".csv"
-
 regex = re.compile('/en/inmueble/*/')
-
 numbers = [details.index(n) for n in details if re.match(regex, n)]
 
 columns_details = ['Flat', 'Bedrooms', 'Square', 'Floor and other details']
@@ -142,53 +116,30 @@ df["Flat_Det"] = flats
 df["Bedrooms"] = bedrooms
 df["Square"] = square
 df["Floor and other details"] = other_details
-street = list()
-area = list()
-number = list()
-street1 = list()
-city = list()
-ftype = list()
+
+df["Street"] = ''
+df["Type"] = ''
+df["Area"] = ''
+df["City"] = ''
 for i in range(0, len(df["Flat"])):
     row = df["Flat"][i].split(',')
-    if len(row) == 2:
-        street.append(row[0])
-        ftype.append(row[0].split("in")[0])
-        city.append(row[1])
-        street1.append("")
-        number.append("")
-        area.append("")
-    elif len(row) == 3:
-        street.append(row[0])
-        ftype.append(row[0].split("in")[0])
-        area.append(row[1])
-        city.append(row[2])
-        street1.append("")
-        number.append("")
-    elif len(row) == 4:
-        street.append(row[0])
-        ftype.append(row[0].split("in")[0])
-        number.append(row[1])
-        area.append(row[2])
-        city.append(row[3])
-        street1.append("")
-    elif len(row) == 5:
-        street.append(row[0])
-        ftype.append(row[0].split("in")[0])
-        street1.append(row[1])
-        number.append(row[2])
-        area.append(row[3])
-        city.append(row[4])
+    df['Street'][i] = row[0].split(" in ")[1]
+    df['Type'][i] = row[0].split(" in ")[0]
+    l = len(row)
+    if l == 2:
+        df["City"][i] = row[1]
+    elif l == 3:
+        df["Area"][i] = row[1]
+        df["City"][i] = row[2]
+    elif l == 4:
+        df["Area"][i] = row[2]
+        df["City"][i] = row[3]
+    elif l == 5:
+        df["Area"][i] = row[3]
+        df["City"][i] = row[4]
     else:
-        street.append(row[0])
-        ftype.append(row[0].split("in")[0])
-        city.append(row[1])
-        street1.append("")
-        number.append("")
-        area.append("")
-df["Street"] = street
-df["Area"] = area
-df["City"] = city
-df["Type"] = ftype
+        df["City"][i] = row[1]
+
 for p in range (0, len(df["Price"])):
     df["Price"][p] = int(df["Price"][p].replace(',', ''))
 for t in range (0, len(df["Flat_Det"])):
@@ -196,6 +147,6 @@ for t in range (0, len(df["Flat_Det"])):
         df["Floor and other details"][t] = df["Square"][t]
         df["Square"][t] = df["Bedrooms"][t]
         df["Bedrooms"][t] = '0 bed'
+st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H%M%S')
+filenamelocation = "C:/Users/maksim.lebedev/Desktop/idealista/idealista-rent-"+st+".csv"
 df.to_csv(filenamelocation, index=False, sep=";")
-df["Price"] = pd.to_numeric(df["Price"])
-avg = df.groupby(["Area"])["Price"].mean()
